48cb9b2be87bd45d64a1e56c075eb55ee21ffabd
Florian Maria Alexandra <sandi.forian@yahoo.com>
2018-06-19 22:55:51 +0300

    add new component

---
+++ android-quickstart-example-master/app/src/main/java/com/back4app/quickstartexampleapp/AddTransport.java
@@ -0,0 +1,13 @@
+package com.back4app.quickstartexampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTransport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_transport);
+    }
+}
